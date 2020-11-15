ff2217790a5ae728ff92fa6d5703392ac4e775b3
Ismael Garcia <00036818@uca.edu.sv>
2020-11-15 00:18:53 -0600

    agregando funcion fetch

--- main.js
+++ main.js
@@ -28,3 +28,75 @@ function mostrarP(){
     document.getElementById('Principal').style.display="block";
 }
 
+const url = 'http://localhost:3000/'
+
+var register = document.getElementById('register')
+
+    register.addEventListener('btn accept',function(e){
+
+    var name = document.getElementById('name').value
+    var email = document.getElementById('email').value
+    var dui = document.getElementById('dui').value
+    var phone = document.getElementById('tel')
+    var username = document.getElementById('user')
+    var password = document.getElementById('pass')
+
+
+    fetch($'{url}',{
+        method: 'POST',
+        body: JSON.stringify({
+
+            username : username,
+            name : name,
+            dui :dui,
+            phone : phone,
+            email : email,
+            password : password,
+        }),
+        headers:{
+            'Content-Type' : 'aplication/json',
+        }  
+    })
+    .then(response => response.json())
+    .then(data => {
+        
+    })
+    .catch(err=>console.log(err) )
+
+})
+
+/*
+var form = document.getElementById('form')
+
+    form.addEventListener('btn accept',function(e){
+
+    var name = document.getElementById('name').value
+    var email = document.getElementById('email').value
+
+  
+
+
+    fetch($'{url}',{
+        method: 'GET',
+        body: JSON.stringify({
+
+            username : username,
+            name : name,
+            dui :dui,
+            phone : phone,
+            email : email,
+            password : password,
+        }),
+        headers:{
+            'Content-Type' : 'aplication/json',
+        }  
+    })
+    .then(response => response.json())
+    .then(data => {
+        
+    })
+    .catch(err=>console.log(err) )
+
+})*/
+
+
